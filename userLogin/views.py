e3cba9a0ec4e0580beb88ac4b72edd9478119dc8
Akash Sharma <35839624+2akash111998@users.noreply.github.com>
2020-04-02 20:21:39 +0530

    Add files via upload

--- userLogin/views.py
+++ userLogin/views.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+	my_dict = { 'insert_content': 'Hello im from userLogin/index.html' }
+	return render(request, 'userLogin/index.html', context = my_dict)
+
+	
